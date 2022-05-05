3736484235ab53b5dc7f6fccfd369a7e30f8adf7
Nikhil Saini <nikhil.saini@prospecta.com>
2022-05-05 17:34:10 +1000

    initial commit

---
+++ apps/e-com/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'e-com-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'e-com';
+}
