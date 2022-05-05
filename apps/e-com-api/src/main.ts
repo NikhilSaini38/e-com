4172da4efe7385cf8670fc9ccda80703d80cc270
Nikhil Saini <nikhil.saini@prospecta.com>
2022-05-06 02:25:31 +1000

    basic nx workflow setup

---
+++ apps/e-com-api/src/main.ts
@@ -0,0 +1,18 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Welcome to e-com-api!' });
+});
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+server.on('error', console.error);
